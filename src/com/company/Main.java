1c184ab4db5487e5fa5c20e2a2bc71050ad94349
Sune1998 <53908735+Sune1998@users.noreply.github.com>
2019-09-17 09:40:33 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+//opgave 2
+    public static void main(String[] args) {
+	// kode til opgave 2
+        int x = 41;
+        int y =  67;
+        int sum = x + y;
+
+
+        //test til opgave 2
+        if (sum >100)
+            System.out.println("summen er større end 100");
+        else System.out.println("sum er mindre end 100");
+
+
+    }
+}
